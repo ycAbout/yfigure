2c1d7462f098e755e2513f4dc6f44fa3d7e35a05
yc <yc.about@gmail.com>
2020-08-31 21:17:34 -0400

    add more demo

--- yfiguresite/public/demo.js
+++
@@ -1,2 +0,0 @@
-    let population = [['Country', 'Population (million)'], ["UK", 68], ["Japan", 126], ["Germany", 84], ["France", 65], ["Italy", 60]];
-    let bar1 = new yf.Bar(population, {location: '#root'})
